c83e9c0047481611dfa43d50fa7478a33304d4d6
DESKTOP-RQRFM9F\wigh1212 <wigh1212@gmail.com>
2025-09-29 16:38:51 +0900

    객체 정리

--- src/main/java/org/eppay/api/EpPayApiApplication.java
+++ src/main/java/org/eppay/api/EpPayApiApplication.java
@@ -3,10 +3,11 @@ package org.eppay.api;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableEncryptableProperties
-public class EpPayApiApplication {
+public class EpPayApiApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EpPayApiApplication.class, args);
