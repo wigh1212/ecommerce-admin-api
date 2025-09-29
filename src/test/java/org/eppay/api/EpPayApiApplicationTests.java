708330b6ba5b86c768ff1d8dd8c3cd0593854606
DESKTOP-RQRFM9F\wigh1212 <wigh1212@gmail.com>
2025-09-29 12:43:24 +0900

    네이밍 정리

--- src/test/java/org/eppay/api/EpPayApiApplicationTests.java
+++ src/test/java/org/eppay/api/EpPayApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StaymoaApiApplicationTests {
+class EpPayApiApplicationTests {
 
 	@Test
 	void contextLoads() {
